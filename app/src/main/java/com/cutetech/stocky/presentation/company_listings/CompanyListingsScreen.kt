2777ef4dc947b35218299d7dd3627d3cb6b320c1
ssindher <shrey.sindher@byjus.com>
2022-08-13 18:13:22 +0530

    Add loader for CompanyListingsScreen

--- app/src/main/java/com/cutetech/stocky/presentation/company_listings/CompanyListingsScreen.kt
+++ app/src/main/java/com/cutetech/stocky/presentation/company_listings/CompanyListingsScreen.kt
@@ -1,16 +1,19 @@
 package com.cutetech.stocky.presentation.company_listings
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -53,6 +56,14 @@ fun CompanyListingsScreen(
                 viewModel.onEvent(CompanyListingsEvent.Refresh)
             }
         ) {
+            if (state.isLoading) {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    CircularProgressIndicator()
+                }
+            }
             LazyColumn(modifier = Modifier.fillMaxSize()) {
                 items(state.companies.size) { i ->
                     val company = state.companies[i]
